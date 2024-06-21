2aad3ccee6c130e387d8097697f5329a2b2b9cc2
alleys <alleys@alleysdeMacBook-Pro.local>
2024-06-21 10:02:35 +0800

    complete abs & relu

--- src/relu.s
+++ src/relu.s
@@ -14,25 +14,26 @@
 # ==============================================================================
 relu:
     # Prologue
-
-
+    ebreak
+    mv t0 a0
+    li t2 1
+    blt a1 t2 err
 loop_start:
-
-
-
-
-
-
-
-
+    # if t2 equal a1 then break loop
+    blt a1 t2 loop_end
+    lw t1 0(t0)
+    bge t1 x0 loop_continue
+    # change negative value to 0 and restore to memory
+    sw x0 0(t0)
 loop_continue:
-
-
-
+    addi t0 t0 4
+    addi t2 t2 1
+    j loop_start
 loop_end:
-
-
-    # Epilogue
-
-
     jr ra
+    # Epilogue
+err:
+    li a0 36
+    j exit
+exit:
+    ecall
